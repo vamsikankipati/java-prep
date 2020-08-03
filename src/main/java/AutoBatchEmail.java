0db37d669164677c06cfc4d6b597a8cc7ee944ca
Vamsi Kankipati <vamsibh1@gmail.com>
2020-08-03 07:37:48 +0530

    Auto email for gmail client

---
+++ src/main/java/AutoBatchEmail.java
@@ -0,0 +1,67 @@
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class AutoBatchEmail {
+    public static void main(String[] args) {
+        System.out.println("Email batch has started...");
+
+        String from = "from@email.com";
+        String pass = "donotharassme";
+        String[] to = { "abc@email.com"
+        }; // list of recipient email addresses
+        String body = "Messaege comes here";
+
+        for (int i = 0; i < 10; i++) {
+            sendEmail(from, pass, to, i, body);
+            System.out.println("Sending Email " + i);
+        }
+    }
+
+    private static void sendEmail(String from, String pass, String[] to, int subjectNo, String body) {
+        String subject = "Hello " + subjectNo;
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+}
