6c812386104bf571ed32be9c93dbb0ca6aaa2cb2
Vamsi Kankipati <vamsibh1@gmail.com>
2023-12-30 13:03:02 +0530

    learnings

--- src/main/java/com/java8/jls/AsyncProgramming.java
+++ src/main/java/com/java8/jls/AsyncProgramming.java
@@ -1,11 +1,24 @@
 package com.java8.jls;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class AsyncProgramming {
 
     // newFixedThreadPool is a factory method
     // Executors produce a factory of methods
-    Executor executor = Executors.newFixedThreadPool(3);
+
+    ExecutorService executorService;
+
+    public static void main(String[] args) {
+        Executor executor = Executors.newFixedThreadPool(1);
+
+        CompletableFuture<String> futureCallable = CompletableFuture.supplyAsync(() -> {
+            return "Run async";
+        }, executor);
+        if (futureCallable.isDone()) {
+            System.out.println("async done");
+        } else if (futureCallable.isCompletedExceptionally()) {
+            System.out.println("async completedExceptionally");
+        }
+    }
 }
---
+++ src/main/java/com/javabasics/Basics.java
@@ -0,0 +1,12 @@
+package com.javabasics;
+
+public class Basics {
+
+    // Strongly typed - variables won't be allowed to hold wrong data types.
+    int a = 10;
+
+    private void assignValue() {
+        // variable a cannot hold any value other than int - strongly typed language
+//        this.a = "a";
+    }
+}
